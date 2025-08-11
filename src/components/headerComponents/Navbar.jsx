baee2ad50b33fd576b3c5cde83ecc529a840dacc
Helal366 <helal56876@gmail.com>
2025-08-11 20:04:34 +0600

    favicon and logo are updated

--- src/components/headerComponents/Navbar.jsx
+++ src/components/headerComponents/Navbar.jsx
@@ -3,29 +3,35 @@ import NavbarLinks from './NavbarLinks';
 
 const Navbar = () => {
     return (
-            <section className="navbar bg-[#1f2632] padding border-b border-gray-300/50 ">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <NavbarLinks />
-                        </ul>
+        <section className="navbar bg-[#1f2632] padding border-b border-gray-300/50 ">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <NavbarLinks />
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn buttonOne">Resume</a>
+                <div className=" text-xl">
+                    <img
+                        src="/logo2.png"
+                        alt="Logo"
+                        className="h-10 w-auto shadow-sm shadow-white rounded-md"
+                    />
                 </div>
-            </section>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <NavbarLinks />
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn buttonOne">Resume</a>
+            </div>
+        </section>
     );
 };
 
