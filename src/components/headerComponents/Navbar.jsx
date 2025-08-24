268a33abd1cbc44d5f3730cb54f12e5235a49499
Helal366 <helal56876@gmail.com>
2025-08-24 19:09:57 +0600

    Theme toggle added.

--- src/components/headerComponents/Navbar.jsx
+++ src/components/headerComponents/Navbar.jsx
@@ -3,13 +3,14 @@ import NavbarLinks from './NavbarLinks';
 import { FaDownload } from 'react-icons/fa';
 
 const Navbar = () => {
-    const resume_file=`https://drive.google.com/uc?export=download&id=${import.meta.env.VITE_RESUME_FILE_ID}`
+    const resume_file = `https://drive.google.com/uc?export=download&id=${import.meta.env.VITE_RESUME_FILE_ID}`
     return (
         <section className="navbar bg-green-300/50 padding border-b-2 border-purple-300/40 ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
                     </div>
                     <ul
                         tabIndex={0}
@@ -17,12 +18,16 @@ const Navbar = () => {
                         <NavbarLinks />
                     </ul>
                 </div>
-                <div className=" text-xl">
+                <div className=" text-xl flex justify-start">
                     <img
                         src="/logo2.png"
                         alt="Logo"
-                        className="h-10 w-auto shadow-sm shadow-white rounded-md"
+                        className="h-10 w-auto shadow-sm shadow-white rounded-md mr-5"
                     />
+
+                    <div>
+                        <input type="checkbox" value="dark" className="toggle theme-controller" />
+                    </div>
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
