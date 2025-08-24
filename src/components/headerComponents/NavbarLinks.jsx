14aad11678d754fa15bbfc1712fe5ca6d75255b5
Helal366 <helal56876@gmail.com>
2025-08-24 12:09:37 +0600

    About option removed from Navbar.

--- src/components/headerComponents/NavbarLinks.jsx
+++ src/components/headerComponents/NavbarLinks.jsx
@@ -5,9 +5,6 @@ const NavbarLinks = () => {
     return (
         <>
             <li><Link to='/'>Home</Link></li>
-            <li>
-                <a href='#about'>About</a>
-            </li>
             <li><a href='#skills'>Skills</a></li>
             <li><a href='#education'>Education</a></li>
             <li><a href='#projects'>Projects</a></li>
