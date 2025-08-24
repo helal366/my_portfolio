e70939ad8e9f3ed772f5d867daf00eb301e2e4bc
Helal366 <helal56876@gmail.com>
2025-08-24 16:39:58 +0600

    Project flexora image changed.

--- src/components/headerComponents/NavbarLinks.jsx
+++ src/components/headerComponents/NavbarLinks.jsx
@@ -5,11 +5,11 @@ const NavbarLinks = () => {
     return (
         <>
             <li className='navLinks'><Link to='/'>Home</Link></li>
-            <li><a href='#skills'>Skills</a></li>
-            <li><a href='#education'>Education</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#contacts'>Contacts</a></li>
-            <li><a href='#aboutme'>About Me</a></li>
+            <li className='navLinks'><a href='#skills'>Skills</a></li>
+            <li className='navLinks'><a href='#education'>Education</a></li>
+            <li className='navLinks'><a href='#projects'>Projects</a></li>
+            <li className='navLinks'><a href='#contacts'>Contacts</a></li>
+            <li className='navLinks'><a href='#aboutme'>About Me</a></li>
         </>
     );
 };
