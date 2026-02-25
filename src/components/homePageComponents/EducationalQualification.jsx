ff6f3103e33b7cac32f4ec14ba397078343a5d1a
Helal366 <helal56876@gmail.com>
2026-02-25 16:44:49 +0600

    educational qualification polished

--- src/components/homePageComponents/EducationalQualification.jsx
+++ src/components/homePageComponents/EducationalQualification.jsx
@@ -3,13 +3,13 @@ import { FaGraduationCap } from "react-icons/fa";
 
 const EducationalQualification = () => {
   return (
-    <section data-aos="fade-up" data-aos-duration="2000" className="section flex flex-col pt-30">
-      <div className="flex items-center gap-3 mb-4">
+    <section data-aos="fade-up" data-aos-duration="2000" className="section flex flex-col pt-30 items-center">
+      <div className="flex items-center gap-3 mb-8">
         <FaGraduationCap className="w-8 h-8 text-purple-500" />
         <h2 className="text-3xl font-semibold">Educational Qualification</h2>
       </div>
-      <div className="pl-2">
-        <h3 className="text-xl font-medium">B.Sc. in Chemical Engineering</h3>
+      <div className="p-4 md:p-8 xl:p-12 rounded-2xl bg-gradient-to-br from-pink-500/70 to-blue-600/70 text-center">
+        <h3 className="text-xl font-medium mb-4">B.Sc. in Chemical Engineering</h3>
         <p className="text-lg">
           Bangladesh University of Engineering and Technology (BUET)
         </p>
