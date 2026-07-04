f8fb800203edc481dbd6d7a459ebed545f1d8af2
Helal366 <helal56876@gmail.com>
2026-07-05 02:12:08 +0600

    educational qualification updated

--- src/components/homePageComponents/EducationalQualification.jsx
+++ src/components/homePageComponents/EducationalQualification.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { FaGraduationCap } from "react-icons/fa";
 
 const EducationalQualification = () => {
+  const [degree, setDegree] = useState([]);
+  useEffect(() => {
+    fetch("/data/education.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setDegree(data);
+      })
+      .catch((err) => console.error("Failed to fetch educations: ", err));
+  }, []);
+  console.log(degree);
   return (
-    <section data-aos="fade-up" data-aos-duration="2000" className="section flex flex-col pt-30 items-center">
-      <div className="flex items-center gap-3 mb-8">
+    <section
+      data-aos="fade-up"
+      data-aos-duration="2000"
+      className="section flex flex-col pt-30 "
+    >
+      <div className="flex items-center gap-3 text-gray-200">
         <FaGraduationCap className="w-8 h-8 text-purple-500" />
-        <h2 className="text-3xl font-semibold">Educational Qualification</h2>
+        <h2 className="text-3xl font-semibold ">Educational Qualification</h2>
       </div>
-      <div className="p-4 md:p-8 xl:p-12 rounded-2xl bg-gradient-to-br from-pink-500/70 to-blue-600/70 text-center">
-        <h3 className="text-xl font-medium mb-4">B.Sc. in Chemical Engineering</h3>
-        <p className="text-lg">
-          Bangladesh University of Engineering and Technology (BUET)
-        </p>
-      </div>
+        <p className="mb-8 text-gray-200">Academic background and core foundations.</p>
+      <section className="flex flex-col  gap-5">
+        {degree && degree.map((deg, index)=>(
+          <div key={index}
+          className="educationCard">
+          <h3 className="text-xl font-medium ">
+            {deg.degree}
+          </h3>
+          <p className="text-lg">
+            {deg.institution}
+          </p>
+        </div>
+        ))}
+        
+      </section>
     </section>
   );
 };
--- src/components/homePageComponents/Projects.jsx
+++ src/components/homePageComponents/Projects.jsx
@@ -11,8 +11,7 @@ const Projects = () => {
       .catch((err) => console.error("Failed to fetch projects:", err));
   }, []);
   return (
-    <section data-aos="fade-up" data-aos-duration="2000" 
-        className="section">
+    <section data-aos="fade-up" data-aos-duration="2000" className="section">
       <div>
         <h2 className="flex items-center gap-3 text-3xl font-semibold mb-8">
           <FaBriefcase className="text-purple-500 w-6 h-6" />
@@ -33,15 +32,32 @@ const Projects = () => {
                 alt="Shoes"
               />
             </figure>
-            <div className="card-body">
+
+            <div className="card-body transition-all duration-300 hover:shadow-lg hover:-translate-y-1">
               <h2 className="card-title text-green-900">{project.name}</h2>
-              <p className="my-2 text-xs">{project.description}</p>
-              <div className="my-3 flex flex-wrap gap-1">{project.techStack.map((tech)=>(
-                <div className="bg-amber-50 px-2 py-1 rounded text-xs">{tech}</div>
-              ))}</div>
+
+              <p className="my-2 text-xs text-gray-600 leading-relaxed">
+                {project.description}
+              </p>
+
+              <div className="relative my-3 overflow-hidden">
+                <div className="marquee flex gap-3 w-max">
+                  {[...project.techStack, ...project.techStack].map(
+                    (tech, index) => (
+                      <span
+                        key={index}
+                        className="bg-amber-50 text-gray-500 px-2 py-1 rounded-full text-xs font-medium shrink-0"
+                      >
+                        {tech}
+                      </span>
+                    ),
+                  )}
+                </div>
+              </div>
+
               <div className="card-actions justify-end">
                 <Link to={`/project/${project.id}`}>
-                  <button className="btn buttonTwo">Details</button>
+                  <button className="btn buttonTwo btn-sm">Details</button>
                 </Link>
               </div>
             </div>
