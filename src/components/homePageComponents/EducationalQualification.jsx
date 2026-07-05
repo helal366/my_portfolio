ac02576bbb7496334fc9740ed60c2f199bc183d0
Helal366 <helal56876@gmail.com>
2026-07-05 10:05:37 +0600

    Education and certificates section updated, added the courses for each certificate.

--- src/components/homePageComponents/EducationalQualification.jsx
+++ src/components/homePageComponents/EducationalQualification.jsx
@@ -20,19 +20,27 @@ const EducationalQualification = () => {
     >
       <div className="flex items-center gap-3 text-gray-200">
         <FaGraduationCap className="w-8 h-8 text-purple-500" />
-        <h2 className="text-3xl font-semibold ">Educational Qualification</h2>
+        <h2 className="text-3xl font-semibold ">Education & Certificates</h2>
       </div>
         <p className="mb-8 text-gray-200">Academic background and core foundations.</p>
-      <section className="flex flex-col  gap-5">
+      <section className="flex flex-col  gap-5  text-gray-500">
         {degree && degree.map((deg, index)=>(
           <div key={index}
           className="educationCard">
           <h3 className="text-xl font-medium ">
             {deg.degree}
           </h3>
-          <p className="text-lg">
+          <p className="text-lg mb-2">
             {deg.institution}
           </p>
+          <div className="flex flex-wrap gap-3 justify-center text-xs">
+            {deg.courses.map((course, index)=>(
+              <div key={index}
+              className="px-1 py-[2px] rounded bg-gray-200">
+                  {course}
+              </div>
+            ))}
+          </div>
         </div>
         ))}
         
